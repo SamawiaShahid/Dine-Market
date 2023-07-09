e2a2efb470c1d1f26f96cdb4ac24c20b9c7d6b81
SamawiaShahid <125427056+SamawiaShahid@users.noreply.github.com>
2023-07-09 15:34:49 +0500

    Update index.tsx

--- my-app/src/siteComponents/promotion/index.tsx
+++ my-app/src/siteComponents/promotion/index.tsx
@@ -9,7 +9,7 @@ const Products = () => {
     <div className="md:my-5 ">
       <div className="title text-center mb-14 lg:px-0 m-5 ">
            <span className="text-[#0062f5] text-sm font-bold">PROMOTIONS</span>
-                <h1 className="text-[#212121] font-bold text-[32px] lg:px-0 px-5 leading-[35px] lg:mt-0 mt-5">
+                <h1 className="text-[#212121] font-bold text-[32px] lg:px-0 px-5 leading-[35px] ">
                       Our Promotions Events</h1>
 
         <div className="banner grid grid-cols-12 gap-2 lg:px-20 py-10 mb-20">
@@ -30,26 +30,26 @@ const Products = () => {
                 DINEWEEKENDSALE</Button>
             </div>
           </div>
-          <div className="lg:col-span-6 md:col-span-5 col-span-12 flex-col lg:flex-row ">
-            <div className="banner-right  md:flex items-center gap-4 mx-2 flex-row lg:px-0 lg:h-full">
-              <div className="banner-right-1 bg-[#efe1c7] h-full flex flex-col justify-between w-full ">
-                <div className=" details box-border font[400] text-[15px] leading-[24px] lg:px-10 px-5  lg:w-[20vw] md:w-60 ">
+         
+            <div className="banner-right flex w-full flex-col space-y-5 md:flex-row md:space-x-5 md:space-y-0 lg:basis-3/5">
+              <div className="banner-right-1 bg-[#efe1c7]  basis-1 flex flex-col justify-between px-6 pt-6 md:basis-1/2 ">
+                <div className=" flex flex-col">
                   <p className=" px-0 text-2xl lg:mt-5 mt-5">Flex Sweatshirt</p>
                   <div className="price">
                     <span className="relative">$100.00 </span>
                     <span className="font-[600] text-[18px] ml-[10px] leading-[23px]">$75.00</span></div>
                   <Image src="/event2.jpg" alt=" " height={218} width={222} className="lg:w-[100%] lg:h-[50vh] h-90vh  object-contain lg:mt-0 mt-5 "></Image></div>
               </div>
-              <div className="banner-right-2 bg-[#d7d7d9] h-full flex flex-col justify-between lg:mt-0 mt-5">
+              <div className="banner-right-2 bg-[#d7d7d9] basis-1 flex flex-col justify-between px-6 pt-6 md:basis-1/2">
                 <div className=" details box-border font[400] text-[15px]  lg:px-10 px-5 lg:w-[20vw] md:w-60 ">
                   <p className=" px-0 text-2xl mt-5">Flex Push Button Boomber</p>
                   <div className="price">
                     <span className="relative ">$225.00 </span>
                     <span className="font-[600] text-[18px] ml-[10px] leading-[23px]">$190.00</span></div>
-                  <Image src="/event3.jpg" alt=" " height={218} width={222} className="lg:w-[100%] lg:h[90vh] h-90vh mt-2 object-contain"></Image></div>
+                  <Image src="/event3.jpg" alt=" " height={362} width={282} className="mx-auto object-contain"></Image></div>
               </div>
 
-            </div>
+           
           </div>
         </div>
       </div></div>
